cab78d537e98a143a163526614625bb3c5de0f2f
Razvan Pascanu <pascanur@simplet.iro.umontreal.ca>
2011-03-02 14:11:40 -0500

    sgd with momentum

--- gd_methods/sgd.py
+++ gd_methods/sgd.py
@@ -7,9 +7,9 @@ my0 = TT.constant(numpy.array(0.0,dtype=theano.config.floatX))
 my1 = TT.constant(numpy.array(1.0,dtype=theano.config.floatX))
 
 
-def sgd(parameters,cost=None,gradients=None, stop=None,
-                        updates=None,lr=2e-2, consider_constant = [],
-                        **kwargs):
+def sgd(parameters,cost=None,gradients=None,
+        updates=None,lr=2e-2, consider_constant = [],
+        momentum = None, **kwargs):
 
     if not isinstance(parameters,list):
         parameters = [parameters]
@@ -23,7 +23,12 @@ def sgd(parameters,cost=None,gradients=None, stop=None,
         if 'scale' in kwargs:
             print 'scaling the lr'
             scale = kwargs['scale']
-        updates[param] =  param - scale*lr * grad
+        if momentum != None:
+            mparam = theano.shared(param.get_value()*0.)
+            updates[param] = param - scale * lr * mparam
+            updates[mparam] = mparam*momentum + (1.-momentum)*grad
+        else:
+            updates[param] =  param - scale*lr * grad
 
     return updates, None
 
